93508f0ce3f9c364ac205b4911da216527851f85
Kausshik Manojkumar <32772050+KAUSSHIK@users.noreply.github.com>
2022-09-12 01:17:14 -0500

    Add MySQL connectivity
    Add MySQL connectivity to improve user experience

--- script.py
+++ script.py
@@ -5,6 +5,35 @@ import time
 from datetime import datetime
 import geocoder
 from geopy.geocoders import Nominatim
+import mysql.connector as mys
+
+#fetch current time
+current_time = time.localtime()
+current_time = time.strftime("%H%M", current_time)
+
+#Create Connection
+mycon = mys.connect(host = 'localhost', user = 'root', passwd = 'PASSWORD', database = 'mydb_01')
+if mycon.is_connected():
+    print("Connection Established")
+else:
+    print("Try again")
+
+#Create MySQL Cursor
+mycur = mycon.cursor()
+mycur.execute("SELECT time FROM ENotifier")
+time_row = [res[0] for res in mycur.fetchall()]
+
+#Function that returns the class/work at the given time
+def return_next_job(row, ctime):
+    min = 100000
+    for r in row:
+        if(0 < r-current_time < min):
+            min = r
+    return min
+
+#Now get the details of what class/job is next on the schedule
+mycur.execute("SELECT Incharge, Email FROM ENotifier WHERE time ={}".format(return_next_job(time_row, current_time)))
+details = mycur.fetchone
 
 #Get the current location using GeoCoder
 curr_loc = geocoder.ip('me')
@@ -42,14 +71,14 @@ def difference(h1, m1, h2, m2):
 with open ("api_key_storage.txt", "r") as api_file:
     api_key = api_file.read()
 
-#Starting point - we will use the address we generated from before
+#Starting point
 starting_point = str(location_name)
 
 #Final Destination
 final_destination = input("Enter your final destination: ")
 
 #Reporting Time
-reporting_time = '...'
+reporting_time = return_next_job(time_row, current_time)
 
 #url to send requests
 url ='https://maps.googleapis.com/maps/api/distancematrix/json?'
@@ -58,20 +87,18 @@ url ='https://maps.googleapis.com/maps/api/distancematrix/json?'
 api_response = requests.get(url + "origins=" + starting_point + "&destinations=" + final_destination + "&key=" + api_key)
 
 
-#fetching time in usual display format and as seconds, we use the seconds for calculation      
+#fetching time in usual display format and as seconds, we use the seconds for calculation
+#time = api_response.json()["rows"][0]["elements"][0]["duration"]["text"]       
 seconds = api_response.json()["rows"][0]["elements"][0]["duration"]["value"]
-print(api_response.json())
+#print(api_response.json())
 
-#fetch current time
-current_time = time.localtime()
-current_time = time.strftime("%H%M", current_time)
 
 if(seconds > difference(int(current_time[0:2]), int(current_time[2:]), int(reporting_time[0:2]), int(reporting_time[2:]))):
     #EMail Credentials
-    sender_email = "..."    
-    recipient_email = "..."       
+    sender_email = "kausshikmanojkumar@gmail.com"    
+    recipient_email = str(details[1])       
     subject = "High Traffic"   
-    message = "Hello Boss,\nSorry, but I can't make it into work today on time.\nThe traffic seems to be very heavy.\nRegards,\n<Your Name>"
+    message = "Hello {},\nSorry, but I can't make it into work today on time.\nThe traffic seems to be very heavy.\nRegards,\nKausshik Manojkumar".format(str(details[0]))
 
     #Formatting the EMail
     email = "Subject: {}\n\n{}".format(subject, message)
@@ -95,10 +122,7 @@ if(seconds > difference(int(current_time[0:2]), int(current_time[2:]), int(repor
 
     #Successfully Sent the Email
     print("Successfully sent the email.")
+    #Close MySQL connection
+    mycon.close()
     #Logout
     s.quit()
-
-
-
-
-
